1c9c5ad8f227fc9fd92c683129515aed07bd3d75
Eric Hua (T&T) <Eric.Hua@tntsupermarket.com>
2022-03-29 16:23:42 -0700

    initial

---
+++ app/shared/src/utils/logger.ts
@@ -0,0 +1,8 @@
+export class logger{
+    constructor(){
+
+    }
+    log(trace: string){
+        console.log(`TEst v3 ${trace}`);
+    }
+}
---
+++ server/libs/src/utils/storage.ts
@@ -0,0 +1,12 @@
+export class Storage{
+    constructor(){
+        console.log('storage contructor');
+    }
+    add(){
+        console.log('storage contructor add+v2');
+    }
+
+    remove(){
+        console.log("storage account is removed + ssdfs");
+    }
+}
